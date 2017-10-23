a92e18bb8ea534245412c4825068c55264c85225
ellery <hzk745@126.com>
2017-10-23 09:28:09 +0800

    implemention of refunding evluator

--- libraries/chain/guard_refund_balance_evaluator.cpp
+++ libraries/chain/guard_refund_balance_evaluator.cpp
@@ -8,7 +8,13 @@ namespace graphene {
 			try
 			{
 			  //adjust the balance of refund_addr
+				database& d = db();
+				const asset_object&   asset_type = o.refund_asset_id(d);
+				const auto refund_addr = o.refund_addr;
+				const auto amount = o.refund_amount;
+				d.adjust_balance(refund_addr,asset(amount,asset_type.get_id()));
 			  //modify the status of transaction
+				//TODO
 			}FC_CAPTURE_AND_RETHROW((o))
 		}
 
@@ -20,7 +26,7 @@ namespace graphene {
 				const asset_object&   asset_type = o.refund_asset_id(d);
 				//if status of last crosschain transaction is Failed,
 				//refund, and need change the status of that status
-
+				//TODO
 			}FC_CAPTURE_AND_RETHROW((o))
 		}
 	}
