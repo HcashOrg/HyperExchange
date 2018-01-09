f8eb93ca3b0f5c5cf8ff7a182ea0785d40b98334
zoowii <1992zhouwei@gmail.com>
2018-01-09 16:16:45 +0800

    add uvm lib

--- setenv_x64.bat
+++ setenv_x64.bat
@@ -1,6 +1,6 @@
  @echo off
-set GRA_ROOT=f:\bts
-set OPENSSL_ROOT=%GRA_ROOT%\OpenSSL-Win64
+set GRA_ROOT=E:\projects
+set OPENSSL_ROOT=%GRA_ROOT%\OpenSSL.x64
 set OPENSSL_ROOT_DIR=%OPENSSL_ROOT%
 set OPENSSL_INCLUDE_DIR=%OPENSSL_ROOT%\include
 set BOOST_ROOT=%GRA_ROOT%\boost_1_60_0
