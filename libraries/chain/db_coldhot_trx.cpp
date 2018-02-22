91d3f76f623f03236e0f6bf7ddd32a7e5e5a5885
Null-nil <530623363@qq.com>
2018-02-22 17:06:32 +0800

    change coldhot Assert into apply

--- libraries/chain/db_coldhot_trx.cpp
+++ libraries/chain/db_coldhot_trx.cpp
@@ -5,9 +5,9 @@ namespace graphene {
 		void database::adjust_coldhot_transaction(transaction_id_type relate_trx_id, transaction_id_type current_trx_id, signed_transaction current_trx, uint64_t op_type) {
 			try {
 				if (op_type == operation::tag<coldhot_transfer_operation>::value) {
-					auto& coldhot_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
-					auto coldhot_tx_iter = coldhot_tx_dbs.find(current_trx_id);
-					FC_ASSERT(coldhot_tx_iter == coldhot_tx_dbs.end(), "This coldhot Transaction exist");
+// 					auto& coldhot_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
+// 					auto coldhot_tx_iter = coldhot_tx_dbs.find(current_trx_id);
+// 					FC_ASSERT(coldhot_tx_iter == coldhot_tx_dbs.end(), "This coldhot Transaction exist");
 					create<coldhot_transfer_object>([&](coldhot_transfer_object& obj) {
 						obj.op_type = op_type;
 						obj.relate_trx_id = transaction_id_type();
@@ -17,14 +17,13 @@ namespace graphene {
 					});
 				}
 				else if (op_type == operation::tag<coldhot_transfer_without_sign_operation>::value){
-					//check original trx exist
 					auto& coldhot_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
 					auto coldhot_tx_iter = coldhot_tx_dbs.find(relate_trx_id);
-					FC_ASSERT(coldhot_tx_iter != coldhot_tx_dbs.end(), "coldhot original trx doesn`t exist");
+					//FC_ASSERT(coldhot_tx_iter != coldhot_tx_dbs.end(), "coldhot original trx doesn`t exist");
 					//check without sign trx exist
-					auto& coldhot_relate_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_relate_trx_id>();
-					auto coldhot_relate_tx_iter = coldhot_relate_tx_dbs.find(current_trx_id);
-					FC_ASSERT(coldhot_relate_tx_iter == coldhot_relate_tx_dbs.end(), "coldhot trx without sign has been created");
+// 					auto& coldhot_relate_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_relate_trx_id>();
+// 					auto coldhot_relate_tx_iter = coldhot_relate_tx_dbs.find(current_trx_id);
+// 					FC_ASSERT(coldhot_relate_tx_iter == coldhot_relate_tx_dbs.end(), "coldhot trx without sign has been created");
 					modify(*coldhot_tx_iter, [&](coldhot_transfer_object& obj) {
 						obj.curret_trx_state = coldhot_without_sign_trx_create;
 					});
@@ -37,12 +36,12 @@ namespace graphene {
 					});
 				}
 				else if (op_type == operation::tag<coldhot_transfer_with_sign_operation>::value) {
-					auto& coldhot_original_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
-					auto coldhot_original_tx_iter = coldhot_original_tx_dbs.find(relate_trx_id);
-					FC_ASSERT(coldhot_original_tx_iter != coldhot_original_tx_dbs.end(), "coldhot original trx doesn`t exist");
-					auto& coldhot_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
-					auto coldhot_tx_iter = coldhot_tx_dbs.find(current_trx_id);
-					FC_ASSERT(coldhot_tx_iter == coldhot_tx_dbs.end(), "coldhot trx with this sign has been created");
+// 					auto& coldhot_original_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
+// 					auto coldhot_original_tx_iter = coldhot_original_tx_dbs.find(relate_trx_id);
+// 					FC_ASSERT(coldhot_original_tx_iter != coldhot_original_tx_dbs.end(), "coldhot original trx doesn`t exist");
+// 					auto& coldhot_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
+// 					auto coldhot_tx_iter = coldhot_tx_dbs.find(current_trx_id);
+// 					FC_ASSERT(coldhot_tx_iter == coldhot_tx_dbs.end(), "coldhot trx with this sign has been created");
 					create<coldhot_transfer_object>([&](coldhot_transfer_object& obj) {
 						obj.op_type = op_type;
 						obj.relate_trx_id = relate_trx_id;
@@ -52,36 +51,50 @@ namespace graphene {
 					});
 				}
 				else if (op_type == operation::tag<coldhot_transfer_combine_sign_operation>::value){
-					auto& coldhot_relate_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
-					auto coldhot_relate_tx_iter = coldhot_relate_tx_dbs.find(relate_trx_id);
-					FC_ASSERT(coldhot_relate_tx_iter != coldhot_relate_tx_dbs.end(), "coldhot original trx doesn`t exist");
+// 					auto& coldhot_relate_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
+// 					auto coldhot_relate_tx_iter = coldhot_relate_tx_dbs.find(relate_trx_id);
+// 					FC_ASSERT(coldhot_relate_tx_iter != coldhot_relate_tx_dbs.end(), "coldhot original trx doesn`t exist");
+// 					auto& coldhot_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
+// 					auto coldhot_tx_iter = coldhot_tx_dbs.find(current_trx_id);
+// 					FC_ASSERT(coldhot_tx_iter == coldhot_tx_dbs.end(), "coldhot trx combine has been created");
+// 					auto coldhot_relate_original_iter = coldhot_tx_dbs.find(coldhot_relate_tx_iter->relate_trx_id);
+// 					FC_ASSERT(coldhot_relate_original_iter != coldhot_tx_dbs.end(), "coldhot original relate trx doesn`t exist");
 					auto& coldhot_tx_dbs = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
-					auto coldhot_tx_iter = coldhot_tx_dbs.find(current_trx_id);
-					FC_ASSERT(coldhot_tx_iter == coldhot_tx_dbs.end(), "coldhot trx combine has been created");
-					auto coldhot_relate_original_iter = coldhot_tx_dbs.find(coldhot_relate_tx_iter->relate_trx_id);
-					FC_ASSERT(coldhot_relate_original_iter != coldhot_tx_dbs.end(), "coldhot original relate trx doesn`t exist");
-					modify(*coldhot_relate_original_iter, [&](coldhot_transfer_object& obj) {
+					auto coldhot_without_sign_trx_iter = coldhot_tx_dbs.find(relate_trx_id);
+					auto coldhot_transfer_trx_iter = coldhot_tx_dbs.find(coldhot_without_sign_trx_iter->relate_trx_id);
+					modify(*coldhot_transfer_trx_iter, [&](coldhot_transfer_object& obj) {
 						obj.curret_trx_state = coldhot_combine_trx_create;
 					});
-					modify(*coldhot_relate_tx_iter, [&](coldhot_transfer_object& obj) {
+					modify(*coldhot_without_sign_trx_iter, [&](coldhot_transfer_object& obj) {
 						obj.curret_trx_state = coldhot_combine_trx_create;
 					});
 					//auto sign_range = get_index_type< coldhot_transfer_index > ().indices().get<by_relate_trxidstate>().equal_range(boost::make_tuple(relate_trx_id, coldhot_sign_trx));
-					std::vector<coldhot_transfer_object> sign_trxs;
-					get_index_type<coldhot_transfer_index >().inspect_all_objects([&](const object& o)
-					{
-						const coldhot_transfer_object& p = static_cast<const coldhot_transfer_object&>(o);
-						if (p.curret_trx_state == coldhot_sign_trx && p.relate_trx_id == relate_trx_id) {
-							sign_trxs.push_back(p);
-						}
-					});
-					for (auto sign_trx_obj : sign_trxs) {
+					auto sign_range = get_index_type<coldhot_transfer_index>().indices().get<by_relate_trx_id>().equal_range(relate_trx_id);
+					for (auto sign_trx_obj : boost::make_iterator_range(sign_range.first,sign_range.second)){
 						auto& sign_tx_db = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
 						auto sign_tx_iter = sign_tx_db.find(sign_trx_obj.current_id);
+						if (sign_tx_iter->curret_trx_state != coldhot_sign_trx){
+							continue;
+						}
 						modify(*sign_tx_iter, [&](coldhot_transfer_object& obj) {
 							obj.curret_trx_state = coldhot_combine_trx_create;
 						});
 					}
+// 					std::vector<coldhot_transfer_object> sign_trxs;
+// 					get_index_type<coldhot_transfer_index >().inspect_all_objects([&](const object& o)
+// 					{
+// 						const coldhot_transfer_object& p = static_cast<const coldhot_transfer_object&>(o);
+// 						if (p.curret_trx_state == coldhot_sign_trx && p.relate_trx_id == relate_trx_id) {
+// 							sign_trxs.push_back(p);
+// 						}
+// 					});
+// 					for (auto sign_trx_obj : sign_trxs) {
+// 						auto& sign_tx_db = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
+// 						auto sign_tx_iter = sign_tx_db.find(sign_trx_obj.current_id);
+// 						modify(*sign_tx_iter, [&](coldhot_transfer_object& obj) {
+// 							obj.curret_trx_state = coldhot_combine_trx_create;
+// 						});
+// 					}
 					auto op = current_trx.operations[0];
 					auto combine_op = op.get<coldhot_transfer_combine_sign_operation>();
 					create<coldhot_transfer_object>([&](coldhot_transfer_object& obj) {
@@ -96,9 +109,9 @@ namespace graphene {
 				else if (op_type == operation::tag<coldhot_transfer_result_operation>::value){
 					auto& coldhot_db_objs = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
 					auto tx_coldhot_without_sign_iter = coldhot_db_objs.find(relate_trx_id);
-					FC_ASSERT(tx_coldhot_without_sign_iter != coldhot_db_objs.end(), "without sign trx doesn`t exist");
+					//FC_ASSERT(tx_coldhot_without_sign_iter != coldhot_db_objs.end(), "without sign trx doesn`t exist");
 					auto tx_coldhot_original_iter = coldhot_db_objs.find(tx_coldhot_without_sign_iter->relate_trx_id);
-					FC_ASSERT(tx_coldhot_original_iter != coldhot_db_objs.end(), "original trx doesn`t exist");
+					//FC_ASSERT(tx_coldhot_original_iter != coldhot_db_objs.end(), "original trx doesn`t exist");
 
 					modify(*tx_coldhot_without_sign_iter, [&](coldhot_transfer_object& obj) {
 						obj.curret_trx_state = coldhot_transaction_confirm;
@@ -106,28 +119,31 @@ namespace graphene {
 					modify(*tx_coldhot_original_iter, [&](coldhot_transfer_object& obj) {
 						obj.curret_trx_state = coldhot_transaction_confirm;
 					});
-					std::vector<coldhot_transfer_object> sign_trxs;
-					get_index_type<coldhot_transfer_index >().inspect_all_objects([&](const object& o)
-					{
-						const coldhot_transfer_object& p = static_cast<const coldhot_transfer_object&>(o);
-						if (p.curret_trx_state == coldhot_combine_trx_create && p.relate_trx_id == relate_trx_id) {
-							sign_trxs.push_back(p);
-						}
-					});
-					for (auto sign_trx_obj : sign_trxs) {
-						auto& sign_tx_db = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
-						auto sign_tx_iter = sign_tx_db.find(sign_trx_obj.current_id);
-						modify(*sign_tx_iter, [&](coldhot_transfer_object& obj) {
-							obj.curret_trx_state = coldhot_transaction_confirm;
-						});
-					}
-// 					auto combine_state_tx_range = get_index_type<coldhot_transfer_index>().indices().get<by_relate_trxidstate>().equal_range(boost::make_tuple(relate_trx_id, coldhot_combine_trx_create));
-// 					for (auto combine_state_tx : boost::make_iterator_range(combine_state_tx_range.first, combine_state_tx_range.second)) {
-// 						auto combine_state_tx_iter = coldhot_db_objs.find(combine_state_tx.current_id);
-// 						modify(*combine_state_tx_iter, [&](coldhot_transfer_object& obj) {
+// 					std::vector<coldhot_transfer_object> sign_trxs;
+// 					get_index_type<coldhot_transfer_index >().inspect_all_objects([&](const object& o)
+// 					{
+// 						const coldhot_transfer_object& p = static_cast<const coldhot_transfer_object&>(o);
+// 						if (p.curret_trx_state == coldhot_combine_trx_create && p.relate_trx_id == relate_trx_id) {
+// 							sign_trxs.push_back(p);
+// 						}
+// 					});
+// 					for (auto sign_trx_obj : sign_trxs) {
+// 						auto& sign_tx_db = get_index_type<coldhot_transfer_index>().indices().get<by_current_trx_id>();
+// 						auto sign_tx_iter = sign_tx_db.find(sign_trx_obj.current_id);
+// 						modify(*sign_tx_iter, [&](coldhot_transfer_object& obj) {
 // 							obj.curret_trx_state = coldhot_transaction_confirm;
 // 						});
 // 					}
+					auto combine_state_tx_range = get_index_type<coldhot_transfer_index>().indices().get<by_relate_trx_id>().equal_range(relate_trx_id);
+					for (auto combine_state_tx : boost::make_iterator_range(combine_state_tx_range.first, combine_state_tx_range.second)) {
+						if (combine_state_tx.curret_trx_state != coldhot_combine_trx_create) {
+							continue;
+						}
+						auto combine_state_tx_iter = coldhot_db_objs.find(combine_state_tx.current_id);
+						modify(*combine_state_tx_iter, [&](coldhot_transfer_object& obj) {
+							obj.curret_trx_state = coldhot_transaction_confirm;
+						});
+					}
 				}
 			}FC_CAPTURE_AND_RETHROW((relate_trx_id)(current_trx_id))
 		}
