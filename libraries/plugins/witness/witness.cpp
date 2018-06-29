e7dde872e6d180250a7f89951a3960576bfb1981
ElleryHao <hzk745@126.com>
2018-06-29 15:56:06 +0800

    roll back the modification

--- libraries/plugins/witness/witness.cpp
+++ libraries/plugins/witness/witness.cpp
@@ -53,6 +53,13 @@ void new_chain_banner( const graphene::chain::database& db )
       "*                              *\n"
       "********************************\n"
       "\n";
+   if( db.get_slot_at_time( fc::time_point::now() ) > 200 )
+   {
+      std::cerr << "Your genesis seems to have an old timestamp\n"
+         "Please consider using the --genesis-timestamp option to give your genesis a recent timestamp\n"
+         "\n"
+         ;
+   }
    return;
 }
 
