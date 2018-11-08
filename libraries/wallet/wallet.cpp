f855d7db8b9d6413e6d01cc3871cdf0f4735e218
ElleryHao <hzk745@126.com>
2018-11-08 16:26:53 +0800

    fix bugs

--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -5471,8 +5471,7 @@ public:
 
       proposal_create_operation prop_op;
 
-      prop_op.expiration_time = expiration_time;
-      prop_op.review_period_seconds = current_params.committee_proposal_review_period;
+      prop_op.expiration_time = fc::time_point_sec(time_point::now()) + fc::seconds(expiration_time);
       prop_op.fee_paying_account = get_account(proposing_account).addr;
 
       prop_op.proposed_ops.emplace_back( update_op );
