6401ec0fec52fe0f667b2f31de2e614a848b3d90
ElleryHao <hzk745@126.com>
2018-12-28 13:55:05 +0800

    modify forking codings

---
+++ libraries/chain/include/graphene/chain/forks.hpp
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2015 Cryptonomex, Inc., and contributors.
+ *
+ * The MIT License
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+#pragma once
+#define ACCOUN_EVALUATE_150000                  150000
+#define DB_MAINT_190000                         190000
+#define DB_MAINT_480000                         480000
+#define ETH_SERI_RECORD_EVALUATE_480000         480000
+#define DB_CROSSCHAIN_TRX_220000                220000
+#define CROSSCHAIN_RECORD_EVALUATE_220000       220000
+#define CROSSCHAIN_RECORD_EVALUATE_480000       480000
+#define CROSSCHAIN_RECORD_EVALUATE_310000       310000
+#define CROSSCHAIN_RECORD_EVALUATE_680000       680000
