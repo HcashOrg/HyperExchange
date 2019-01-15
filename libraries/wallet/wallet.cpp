36d83fcf02a0f05a6be5705d3462818a4910b170
ElleryHao <hzk745@126.com>
2019-01-15 15:00:16 +0800

    modify version

--- libraries/wallet/wallet.cpp
+++ libraries/wallet/wallet.cpp
@@ -623,7 +623,7 @@ public:
       result["head_block_age"] = fc::get_approximate_relative_time_string(dynamic_props.time,
                                                                           time_point_sec(time_point::now()),
                                                                           " old");
-	  result["version"] = "1.2.9";
+	  result["version"] = "1.2.10";
       result["next_maintenance_time"] = fc::get_approximate_relative_time_string(dynamic_props.next_maintenance_time);
       result["chain_id"] = chain_props.chain_id;
 	  //result["data_dir"] = (*_remote_local_node)->get_data_dir();
