646966e2551230a21973488bcf78719226303797
ElleryHao <hzk745@126.com>
2019-01-30 16:15:08 +0800

    fix coredump bugs

--- libraries/crosschain/crosschain_transaction_record_plugin.cpp
+++ libraries/crosschain/crosschain_transaction_record_plugin.cpp
@@ -358,8 +358,8 @@ namespace graphene {
 		void crosschain_record_plugin::plugin_startup(){
 			try {
 				if (!_miners.empty() || !_guard.empty()) {
-					auto fut = _thread.async([&] {schedule_acquired_record_loop(); }, "crosschain_record_plugin");
-					fut.wait();
+					_fut = _thread.async([&] {schedule_acquired_record_loop(); }, "crosschain_record_plugin");
+					_fut.wait();
 					started = true;
 				}
 				else {
@@ -381,6 +381,8 @@ namespace graphene {
 		}
 		void crosschain_record_plugin::plugin_shutdown(){
 			try {
+				_acquire_crosschain_task.cancel();
+				_fut.cancel();
 				//_thread.quit();
 			}FC_CAPTURE_AND_RETHROW()
 			return;
