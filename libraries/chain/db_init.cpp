99302f626c72211cd505b51173f577a1142406c7
ElleryHao <hzk745@126.com>
2019-02-18 13:16:29 +0800

    add new object

--- libraries/chain/db_init.cpp
+++ libraries/chain/db_init.cpp
@@ -128,6 +128,9 @@ const uint8_t bonus_object::type_id;
 
 const uint8_t eth_multi_account_trx_object::space_id;
 const uint8_t eth_multi_account_trx_object::type_id;
+const uint8_t whiteOperationList_object::space_id;
+const uint8_t whiteOperationList_object::type_id;
+
 void database::initialize_evaluators()
 {
    _operation_evaluators.resize(255);
@@ -313,6 +316,7 @@ void database::initialize_indexes()
    add_index<primary_index<eth_multi_account_trx_index>>();
    add_index<primary_index<referendum_index>>();
    add_index<primary_index<blocked_index>>  ();
+   add_index<primary_index<whiteOperation_index>>();
 }
 
 void database::init_genesis(const genesis_state_type& genesis_state)
