c83210d0d6dc31531fd9e24d8aca73f6499f2925
zoowii <1992zhouwei@gmail.com>
2019-03-14 03:29:33 +0000

    fetch submodules in build

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ project_dir=`pwd`
 container_name="hxcoredevci"
 docker run -t -d -v $project_dir:/code --name $container_name $image /bin/bash
 echo $container_name
+docker exec $container_name git submodule update --init --recursive
 docker exec $container_name cmake .
 docker exec $container_name ./build_node.sh
 
