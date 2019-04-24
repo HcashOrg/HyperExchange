22f2b6aa2808e6a76a121b0d1042c75f638e8842
ElleryHao <hzk745@126.com>
2019-04-24 16:56:30 +0800

    add new interface

--- libraries/app/include/graphene/app/database_api.hpp
+++ libraries/app/include/graphene/app/database_api.hpp
@@ -408,6 +408,7 @@ class database_api
        */
       vector<call_order_object> get_margin_positions( const account_id_type& id )const;
 	  vector<vote_result_object> get_vote_result_objs(const vote_object_id_type& id) const;
+	  vector<vote_object> get_votes_by_addr(const address& addr) const;
       /**
        * @brief Request notification when the active orders in the market between two assets changes
        * @param callback Callback method which is called when the market changes
@@ -866,4 +867,5 @@ FC_API(graphene::app::database_api,
 	(get_midware_eps)
 	(get_contract_storage)
 	(get_vote_result_objs)
+	(get_votes_by_addr)
 );
