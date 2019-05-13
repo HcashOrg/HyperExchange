faa27967f0af0da1b847dde599ad75767013c086
Null-nil <530623363@qq.com>
2019-05-13 16:38:09 +0800

    change localtest

--- libraries/chain/db_update.cpp
+++ libraries/chain/db_update.cpp
@@ -270,10 +270,8 @@ void database::determine_referendum_detailes()
 			{
 				if (itr.get_id() == asset_id_type())
 					continue;
-				if(head_block_num() > 200000){
-				    if (itr.symbol == "ETH" || itr.symbol.find("ERC") != string::npos){
-                                        continue;
-				    }
+				if (itr.symbol == "ETH" || itr.symbol.find("ERC") != string::npos){
+                                    continue;
 				}
 				auto multisig_account_pair = get_current_multisig_account(itr.symbol);
 				if (!multisig_account_pair.valid())
